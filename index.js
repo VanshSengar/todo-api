2520470feabcd525c0d9022a04bd02840e2417cf
vansh sengar <vansh274000@gmail.com>
2023-07-10 13:04:48 +0530

    added new feature 3

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ require('dotenv').config();
 const PORT = process.env.PORT ||4000;
 
 app.use(express.json());
+
+require ("./config/database").connect();
+
+const user = require("./routes/user")
+app.use("/api/v1", user);
+
+//activate
+
+app.listen(PORT, () => {
+    console.log(`App is listening at ${PORT}`);
+})
