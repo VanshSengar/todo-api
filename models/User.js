f9186aa9f3f987699afeeb12f0d899406bedd707
vansh sengar <vansh274000@gmail.com>
2023-07-10 22:19:48 +0530

    isEmailVerify is added

--- models/User.js
+++ models/User.js
@@ -1,42 +1,47 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-    // length
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      // length
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      // length min/max
+    },
+    gender: {
+      type: String,
+      //enum ['male', "female","not to prefer"]
+    },
+    roles: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Role',
+    },
+    mobile: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isEmailVerify: { type: 'boolean', default: false },
+    _isDeleted: { type: 'boolean', default: false }, //soft delete
+    token: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-     // length min/max
-  },
-  gender:{
-    type: String,
-    //enum ['male', "female","not to prefer"]
-  },
-  roles:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref : 'Role'
-  },
-  mobile:{
-     type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  _isDeleted :  { type: "boolean", default: false }, //soft delete
-  token: {
-    type: String,
-  },    
-}, { timestamps: true })
+  { timestamps: true },
+)
 
 module.exports = mongoose.model(user, userSchema)
